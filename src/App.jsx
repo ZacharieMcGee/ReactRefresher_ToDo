08061510c85d2593c0e183a2f9a8fe871b362728
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2024-06-19 19:00:20 -0400

    React Refresher Course 1 - Todo App Init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Todoinput from "./components/Todoinput"
+import Todolist from "./components/Todolist"
+
+function App() {
+
+  return (
+    <main>
+      <Todoinput />
+      <Todolist />
+    </main>
+  )
+}
+
+export default App
